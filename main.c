76e325495c0417be394bb33f31b781c8fc11b860
akim <169460914+v49480nd-km@users.noreply.github.com>
2025-10-17 17:50:30 -0400

    using kernel style guide to embrace the true ways of lower level programming

--- main.c
+++ main.c
@@ -3,98 +3,99 @@
 #include <string.h>
 #include <time.h>
 
-#define CHARS "abcdefghijklmnopwqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" // 52
-#define NUMS "1234567890" // 10
 #define PWD_SIZE 18
-#define SYMBOLS "!@#$%^&*()" // 10
 
 /* TODO
  * STORE PASSWORD WITH NAME
+ * LIST PASSWORDS
  * SEARCH FOR PASSWORD GIVEN NAME
  * DELETE PASSWORD GIVEN NAME
  * PROFIT
  */
 
-void createPassword(char* pwd) {
-    if (pwd == NULL) {
-        printf("CHAR POINTER ERROR\n");
-        exit(EXIT_FAILURE);
-    }
+void createPassword(char* pwd)
+{
+        const char[52] CHARS "abcdefghijklmnopwqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const char[10] NUMS "1234567890";
+        const char[10] SYMBOLS "!@#$%^&*()";
+        int random = rand() % 3;
 
-    int random;
-
-    random = rand() % 3;
-
-    for (int i = 0; i < PWD_SIZE; i++) {
-        if (random == 0) {
-            pwd[i] = CHARS[rand() % 52];
-            random = rand() % 3;
-            continue;
-        } else if (random == 1) {
-            pwd[i] = NUMS[rand() % 10];
-            random = rand() % 3;
-            continue;
-        } else if (random == 2) {
-            pwd[i] = SYMBOLS[rand() % 10];
-            random = rand() % 3;
-            continue;
+        for (int i = 0; i < PWD_SIZE; i++) {
+                if (random == 0) {
+                    pwd[i] = CHARS[rand() % 52];
+                    random = rand() % 3;
+                    continue;
+                } else if (random == 1) {
+                    pwd[i] = NUMS[rand() % 10];
+                    random = rand() % 3;
+                    continue;
+                } else if (random == 2) {
+                    pwd[i] = SYMBOLS[rand() % 10];
+                    random = rand() % 3;
+                    continue;
+                }
         }
-    }
 
-    printf("%s\n", pwd);
-    free(pwd);
+        printf("%s\n", pwd);
+        free(pwd);
 }
 
-int main() {
-    int attempts = 5;
-    int option;
+int main()
+{
+        int attempts = 5;
+        int option;
 
-    option_loop:
+        option_loop:
 
-    printf(
-        "PWDMGR\n\n"
-        "[1] Create\n"
-        "[2] Find\n"
-        "[3] Delete\n"
-        "[4] Exit\n\n"
-        ">>> "
-    );
+        printf(
+                "PWDMGR\n\n"
+                "[1] Create\n"
+                "[2] Find\n"
+                "[3] Delete\n"
+                "[4] Exit\n\n"
+                ">>> "
+        );
 
-    while (!option || option <= 0 || option > 4) {
-        scanf("%d", &option);
+        while (!option || option <= 0 || option > 4) {
+                scanf("%d", &option);
 
-        switch (option) {
-            case 1:
-                char* new_pwd = (char*)malloc(sizeof(char) * PWD_SIZE);
+                switch (option) {
+                case 1:
+                        char* new_pwd = (char*)malloc(sizeof(char) * PWD_SIZE);
 
-                printf("CREATE\n");
-                srand(time(NULL));
-                createPassword(new_pwd);
+                        if (new_pwd == NULL) {
+                                printf("ALLOCATION ERROR! EXITING PROGRAM!\n");
+                                exit(EXIT_FAILURE);
+                        }
 
-                option = 0;
+                        printf("CREATE\n");
+                        srand(time(NULL));
+                        createPassword(new_pwd);
 
-                goto option_loop;
-            case 2:
-                printf("FIND\n");
+                        option = 0;
 
-                option = 0;
+                        goto option_loop;
+                case 2:
+                        printf("FIND\n");
 
-                goto option_loop;
-            case 3:
-                printf("DELETE\n");
+                        option = 0;
 
-                option = 0;
+                        goto option_loop;
+                case 3:
+                        printf("DELETE\n");
 
-                goto option_loop;
-            case 4:
-                printf("EXITING...\n");
-                exit(0);
-            default:
-                printf("Please try again>>> ");
-                break;
+                        option = 0;
+
+                        goto option_loop;
+                case 4:
+                        printf("EXITING...\n");
+                        exit(0);
+                default:
+                        printf("Please try again>>> ");
+                        break;
+                }
         }
-    }
 
 
-    return 0;
+        return 0;
 }
