dc68748ecd6e4b40eea2da2ca283dd5229c5fec9
akim <169460914+v49480nd-km@users.noreply.github.com>
2025-10-18 17:10:56 -0400

    got termnial flags working after some github research

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 #include <string.h>
 #include <time.h>
 
+#define MAJOR 0
+#define MINOR 1
+#define PATCH 1
 #define PWD_SIZE 18
 
 /* TODO
@@ -12,14 +15,8 @@
    SEARCH FOR PASSWORD GIVEN NAME
    DELETE PASSWORD GIVEN NAME
    MASTER PASSWORD
+   ENCRYPTION
    PROFIT */
-
-/* COMMANDS
-   -h -> help
-   -c -> create
-   -l -> list
-   -f <name> -> find
-   -d <name> -> find */
 
 void createPassword(char* pwd)
 {
@@ -50,38 +47,76 @@ void createPassword(char* pwd)
 
 int main(int argc, char* argv[])
 {
-        if (argc <= 1)
-                printf("Not a usable command, please use pwdmgr -h for help\n");
+        for (int i = 1; i < argc; i++) {
+                if (strcmp(argv[i], "-c") == 0 || strcmp(argv[i], "--create") == 0) {
+                        char* new_pwd = (char*)malloc(sizeof(char) * PWD_SIZE);
 
-        if (argv[1] == "-c") {
-                char* new_pwd = (char*)malloc(sizeof(char) * PWD_SIZE);
+                        if (new_pwd == NULL) {
+                                printf("Failure creating password, exiting program\n");
+                                exit(EXIT_FAILURE);
+                        }
 
-                if (new_pwd == NULL) {
-                        printf("ALLOCATION ERROR! EXITING PROGRAM!\n");
-                        exit(EXIT_FAILURE);
+                        printf("Generating Password: ");
+                        srand(time(NULL));
+                        createPassword(new_pwd);
+                } else if (strcmp(argv[i], "-d") == 0 || strcmp(argv[i], "--delete") == 0) {
+                        char* name = (char*)malloc(sizeof(char) * 32);
+                        printf("Input name to delete: ");
+                        scanf("%32s", name);
+
+                        if (name == NULL) {
+                                printf("Failure getting name, exiting program\n");
+                                exit(EXIT_FAILURE);
+                        }
+
+                        char* name2 = (char*)realloc(name, strlen(name+1) * sizeof(char));
+
+                        if (name2 == NULL) {
+                                printf("Failure getting name, exiting program\n");
+                                exit(EXIT_FAILURE);
+                        }
+
+                        name = name2;
+                        printf("Deleting: %s\n", name);
+                        free(name);
+                } else if (strcmp(argv[i], "-f") == 0 || strcmp(argv[i], "--find") == 0) {
+                        char* name = (char*)malloc(sizeof(char) * 32);
+                        printf("Input name to find: ");
+                        scanf("%32s", name);
+
+                        if (name == NULL) {
+                                printf("Failure getting name, exiting program\n");
+                                exit(EXIT_FAILURE);
+                        }
+
+                        char* name2 = (char*)realloc(name, strlen(name+1) * sizeof(char));
+
+                        if (name2 == NULL) {
+                                printf("Failure getting name, exiting program\n");
+                                exit(EXIT_FAILURE);
+                        }
+
+                        name = name2;
+                        printf("Finding: %s\n", name);
+                        free(name);
+                } else if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
+                        printf(
+                                "-c, --create -> creates a password and stores it automatically\n"
+                                "-d, --delete -> given a name finds and deletes password from storage\n"
+                                "-f, --find -> given a name finds that specific password\n"
+                                "-h, --help -> displays this very menu\n"
+                                "-l, --list -> lists all passwords\n"
+                                "-v, --version -> displays current version\n"
+                        );
+                } else if (strcmp(argv[i], "-l") == 0 || strcmp(argv[i], "--list") == 0) {
+                        printf("Listing passwords\n");
+                } else if (strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "--version") == 0) {
+                        printf("Version %d.%d.%d\n", MAJOR, MINOR, PATCH);
+                } else if (argv[i] == NULL) {
+                        printf("Not an option please use pwdmgr -h for more help\n");
+                } else {
+                        printf("Not an option please use pwdmgr -h for more help\n");
                 }
-
-                printf("CREATE\n");
-                srand(time(NULL));
-                createPassword(new_pwd);
-        } else if (argv[1] == "-d") {
-                printf("DELETE\n");
-        } else if (argv[1] == "-f") {
-                printf("FINDING\n");
-        } else if (argv[1] == "-h") {
-                help_section:
-                printf(
-                        "-h -> help\n"
-                        "-c -> create passwords\n"
-                        "-l -> list passwords\n"
-                        "-f <name> -> find password given name\n"
-                        "-d <name> -> delete password based off name\n"
-                );
-        } else if (argv[1] == "-l") {
-                printf("LISTING\n");
-        } else {
-                printf("Not a usable command, please use pwdmgr -h for help\n");
-                goto help_section;
         }
 
         return 0;
